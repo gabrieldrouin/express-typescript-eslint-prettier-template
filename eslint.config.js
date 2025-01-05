def31e53038fe7468f07e2cf00fba8dbbb69a4ac
Gabriel Drouin <info@gabrieldrouin.com>
2025-01-05 13:19:38 -0500

    feat: add vitest & update .gitignore

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@
 
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
+import vitest from "@vitest/eslint-plugin";
 
 export default tseslint.config(
   {
@@ -16,6 +17,15 @@ export default tseslint.config(
         projectService: true,
         tsconfigRootDir: import.meta.dirname,
       },
+    },
+  },
+  {
+    files: ["**/*.test.ts", "**/*.spec.ts"],
+    plugins: {
+      vitest,
+    },
+    rules: {
+      ...vitest.configs.recommended.rules,
     },
   },
 );
